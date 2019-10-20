8ea18aa64855134ff11004611579c1e2fd5c467a
jhiekkap <jari.hiekkapelto@gmail.com>
2019-10-20 20:44:26 +0300

    upload

--- src/components/UploadCSV.js
+++ src/components/UploadCSV.js
@@ -14,6 +14,7 @@ const endpoint = 'http://localhost:3001/'
 
 const UploadCSV = () => {
   const [upLoadedCSV, setUpLoadedCSV] = useState([])
+  const [newTableName, setNewTableName] = useState('perse')
 
   const handleUploadFile = () => {
     const file = document.getElementById('uploadedFile')
@@ -23,30 +24,72 @@ const UploadCSV = () => {
       reader.onload = e => {
         let text = e.target.result
         text = text.replace(/Ã¤/g, 'ä')
-        text = text.replace(/Ã¶/g, 'ö')  
+        text = text.replace(/Ã¶/g, 'ö')
 
         const uploadedRows = text.split('\n')
-        let splitter = ','
-        if(uploadedRows[0].split(';').length > uploadedRows[0].split(',').length){
-            splitter = ';'
-        }
+        const delimiter =
+          uploadedRows[0].split(';').length > uploadedRows[0].split(',').length
+            ? ';'
+            : ','
         //console.log(uploadedRows)
         let CSVrows = []
-        uploadedRows.forEach(row => CSVrows.push(row.split(splitter)))
+        uploadedRows.forEach(row => CSVrows.push(row.split(delimiter)))
         //console.log(CSVrows)
-        setUpLoadedCSV(CSVrows) 
-        CSVrows[0].forEach(cell=>console.log(isNaN(cell)))
+        setUpLoadedCSV(CSVrows)
+        //CSVrows[0].forEach(cell=>console.log(isNaN(cell)))
       }
       reader.readAsText(file.files[0])
+    }
+  }
+
+  const handleSaveFile = async () => {
+    console.log('TRYING TO SAVE')
+    if (newTableName) {
+      const columns = upLoadedCSV[0].map(col => ({
+        name: col,
+        type: isNaN(col) ? 'varchar(20)' : 'int',
+      }))
+      const table = upLoadedCSV.slice(1)
+      console.log('TABLE CONTENTS', table)
+
+      try {
+        let response = await fetch(endpoint + 'create', {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'post',
+          body: JSON.stringify({ newTableName, columns, table }),
+        })
+        console.log(response)
+        if (response.status === 422) {
+          alert('invalid input')
+        }
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
   return (
     <Container>
       <Row>
-        <Form onChange={handleUploadFile}>
-          <Form.Label>Upload CSV</Form.Label>
-          <Form.Control type='file' id='uploadedFile' />
-        </Form>
+        <Col>
+          <Form>
+            <Form.Label>Upload CSV</Form.Label>
+            <Form.Control
+              type='file'
+              id='uploadedFile'
+              onChange={handleUploadFile}
+            />
+          </Form>
+        </Col>
+        {upLoadedCSV.length > 0 && (
+          <Col>
+            <Button variant='outline-secondary' onClick={handleSaveFile}>
+              SAVE TO DATABASE
+            </Button>
+          </Col>
+        )}
       </Row>
       <Row>
         <Table striped bordered hover>
