30fb56596d9386c5b22b157ddced0050fd03df4d
jhiekkap <jari.hiekkapelto@gmail.com>
2019-10-21 20:07:59 +0300

    clean & save new table

--- src/components/UploadCSV.js
+++ src/components/UploadCSV.js
@@ -1,56 +1,89 @@
 import React, { useState } from 'react'
-import {
-  Button,
-  Container,
-  Col,
-  Row,
-  Table,
-  Dropdown,
-  DropdownButton,
-  Form,
-} from 'react-bootstrap'
-import axios from 'axios'
+import { Button, Container, Col, Row, Table, Form } from 'react-bootstrap'
 const endpoint = 'http://localhost:3001/'
 
 const UploadCSV = () => {
   const [upLoadedCSV, setUpLoadedCSV] = useState([])
   const [newTableName, setNewTableName] = useState('excelpaatteita')
 
+  function empty(data) {
+    if (typeof data == 'number' || typeof data == 'boolean') {
+      return false
+    }
+    if (typeof data == 'undefined' || data === null) {
+      return true
+    }
+    if (typeof data.length != 'undefined') {
+      return data.length == 0
+    }
+    var count = 0
+    for (var i in data) {
+      if (data.hasOwnProperty(i)) {
+        count++
+      }
+    }
+    return count == 0
+  }
+
   const handleUploadFile = () => {
     const file = document.getElementById('uploadedFile')
-    console.log(file.files[0].type)
+    const type = file.files[0].type
+    const name = file.files[0].name.split('.')[0]
+    console.log(type)
+    console.log(name)
+    setNewTableName(name)
     //if (file.files[0].type === 'text/csv') {
-      const reader = new FileReader()
+    const reader = new FileReader()
 
-      reader.onload = e => {
-        let text = e.target.result
-        text = text.replace(/Ã¤/g, 'ä')
-        text = text.replace(/Ã¶/g, 'ö')
+    reader.onload = e => {
+      let text = e.target.result
+    
+      text = text.replace(/Ã¤/g, 'ä')
+      text = text.replace(/Ã¶/g, 'ö')
 
-        const uploadedRows = text.split('\n')
-        const delimiter =
-          uploadedRows[0].split(';').length > uploadedRows[0].split(',').length
-            ? ';'
-            : ','
-        //console.log(uploadedRows)
-        let CSVrows = []
-        uploadedRows.forEach(row => CSVrows.push(row.split(delimiter)))
-        //console.log(CSVrows)
-        
-        setUpLoadedCSV(CSVrows)
-        //CSVrows[0].forEach(cell=>console.log(isNaN(cell)))
+      const uploadedRows = text.split('\n')
+      const delimiter =
+        uploadedRows[0].split(';').length > uploadedRows[0].split(',').length
+          ? ';'
+          : ','
+      console.log(uploadedRows)
+      let CSVrows = []
+      uploadedRows.forEach(row => CSVrows.push(row.trim().split(delimiter)))
+      //console.log(CSVrows)
+
+      let cleanerCSV = CSVrows.filter(row => row.length > 1)
+      while (true) {
+        let lastCol = cleanerCSV[0][cleanerCSV[0].length - 1]
+        console.log('LAST COL:', lastCol)
+        console.log(typeof lastCol, lastCol.length, lastCol === '')
+        if (empty(lastCol) || lastCol === '' || lastCol.length < 2) {
+          console.log('hep')
+          cleanerCSV = cleanerCSV.map(row => {
+            let rowToPop = row
+            rowToPop.pop()
+            console.log('POPPED ROW', rowToPop)
+            return rowToPop
+          })
+        } else {
+          console.log('BREAK!')
+          break
+        }
       }
+      console.log(cleanerCSV)
+      setUpLoadedCSV(cleanerCSV)
+      cleanerCSV[1].forEach(cell => console.log(!isNaN(cell)))
+    }
 
-      reader.readAsText(file.files[0])
-   // }
+    reader.readAsText(file.files[0])
+    // }
   }
 
   const handleSaveFile = async () => {
     console.log('TRYING TO SAVE')
     if (newTableName) {
-      const columns = upLoadedCSV[0].map(col => ({
+      const columns = upLoadedCSV[0].map((col, i) => ({
         name: col,
-        type: isNaN(col) ? 'varchar(20)' : 'int',
+        type: isNaN(upLoadedCSV[0][i]) ? 'varchar(256)' : 'int',
       }))
       const table = upLoadedCSV.slice(1)
       console.log('TABLE CONTENTS', table)
@@ -96,16 +129,23 @@ const UploadCSV = () => {
       </Row>
       <Row>
         <Table striped bordered hover>
-          <tbody>
-            {upLoadedCSV &&
-              upLoadedCSV.map((row, r) => (
+          {upLoadedCSV[0] && (
+            <tbody>
+              <tr><td></td>
+                {upLoadedCSV[0].map((row, r) => (
+                  <td key={r}>x</td>
+                ))}
+              </tr>
+              {upLoadedCSV.map((row, r) => (
                 <tr key={r}>
+                  <td>x</td>
                   {row.map((cell, c) => (
                     <td key={c}>{cell}</td>
                   ))}
                 </tr>
               ))}
-          </tbody>
+            </tbody>
+          )}
         </Table>
       </Row>
     </Container>
