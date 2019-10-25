dc1987f7237d823006a6d9daf56174686657a43f
jhiekkap <jari.hiekkapelto@gmail.com>
2019-10-25 12:14:40 +0300

    +?

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -54,9 +54,10 @@ const Tables = ({
     /* const scandies = ['/ä/g','/Ä/g','/ö/g','/Ö/g','/å/g','/Å/g']
     const nonScandies = ['a','A','o','O','å','Å']
     nonScandies.forEach((char,i) => name.replace(scandies[i], char)) */
-    ;['a', 'A', 'o', 'O', 'å', 'Å'].forEach((char, i) =>
+    ['a', 'A', 'o', 'O', 'å', 'Å'].forEach((char, i) =>
       name.replace(['/ä/g', '/Ä/g', '/ö/g', '/Ö/g', '/å/g', '/Å/g'][i], char)
     )
+    name = name.replace(/€\//g, 'ePer')
     name = name.replace(/[^a-zA-Z0-9 ]/g, '')
     return name.replace(/ /gi, '_')
   }
